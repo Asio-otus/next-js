b596af025096c243b770967e991e1baba4a3a92d
Mark Kadiner <dasistmail765@gmail.com>
2021-01-22 17:33:19 +0300

    unnamed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Front page</h1>
-    </div>
-  )
+import fetch from 'isomorphic-unfetch'
+
+export default function Home({ movies }) {
+    console.log(movies)
+    return (
+        <div className='container'>
+            <h1>Front page</h1>
+        </div>
+    )
+}
+
+export async function getServerSideProps() {
+    const { API_URL } = process.env
+
+    const res = await fetch(`${API_URL}/movies`)
+    const data = await res.json()
+
+    return {
+        props: {
+            movies: data
+        }
+    }
 }
